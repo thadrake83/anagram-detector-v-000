4cd9f4014f33c8bc4d4d5df616de0699e50ec323
Drake Rentrop <drakerentrop@gmail.com>
2019-03-05 17:16:13 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array_of_words)
+    array_of_words.select do |x|
+      (@word.split("").sort) == (x.split("").sort)
+    end
+  end
+end
